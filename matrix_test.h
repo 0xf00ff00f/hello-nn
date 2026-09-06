c3668f9299e809d2f6fbee090e1839950022e90b
Mauro Persano <fzorto@gmail.com>
2026-09-06 19:52:31 -0300

    Fix subtraction test

--- matrix_test.h
+++ matrix_test.h
@@ -141,6 +141,7 @@ static_assert([] {
     Matrixf a{ 3, 2, { 1, 2, 3, 4, 5, 6 } };
     Matrixf b{ 3, 2, { 7, 8, 9, 10, 11, 12 } };
     Matrixf c = a - b;
+    assert((c == Matrixf{ 3, 2, { -6, -6, -6, -6, -6, -6 } }));
     return true;
 }());
 
